9e3b2e2d6a66ed3b2eb34520a6ef2b1be2cd106e
Zachary Boelter <zboelter@gmail.com>
2022-04-20 13:13:12 -0500

    add author to dispaly

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-function getBackGroundImage() {
-    fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
-        .then(res => res.json())
-        .then(data => {
-            document.body.style.backgroundImage = `url(${data.urls.full})`
-        })
-}
+const author = document.getElementById('author-name')
 
-getBackGroundImage()
+
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        author.textContent = `By: ${data.user.name}`
+    })
+
+
+
