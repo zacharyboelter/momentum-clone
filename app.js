4ed0ef1f4762eea0435b4e3282916c91054dfbe6
Zachary Boelter <zboelter@gmail.com>
2022-04-20 14:12:22 -0500

    catch error default image from API

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const author = document.getElementById('author')
 
 
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=natu747474747474747474re')
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
         author.textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+        
     })
 
 
