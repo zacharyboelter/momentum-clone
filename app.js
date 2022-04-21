f23ea65f96c49ef6dc00fa28b48576b2ddce4d63
Zachary Boelter <zboelter@gmail.com>
2022-04-21 12:54:41 -0500

    geolocate user and plug info into openweathermap api to get weather

--- app.js
+++ app.js
@@ -43,10 +43,23 @@ fetch('https://api.coingecko.com/api/v3/coins/ethereum')
 
 function getTime() {
     let d = new Date();
-    let n = d.toLocaleTimeString("en-us", {timeStyle: "short"});
+    let n = d.toLocaleTimeString("en-us", { timeStyle: "short" });
     document.getElementById('time').textContent = n
 }
 setInterval(getTime, 1000)
 
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Weather data not available')
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.error(err))
+});
 
 
