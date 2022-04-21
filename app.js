ca2d6df472be2f7defd8627e5b7cd7b6ec2914e8
Zachary Boelter <zboelter@gmail.com>
2022-04-21 13:28:08 -0500

    round temp to nearest int, display name of closest "station"

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ navigator.geolocation.getCurrentPosition(position => {
 
             document.getElementById('weather').innerHTML = `
                 <img src="${iconImage}" />
-                
+                <p>${Math.round(data.main.temp)}º</p>
+                <p>${data.name}</p>
             `
         })
         .catch(err => console.error(err))
